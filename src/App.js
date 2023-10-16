95e8872d01132c76b62ba49f3497a859a8b7100e
siddhaarthh <siddharthjain418@gmail.com>
2023-10-16 23:43:37 +0530

    add

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Comp from './comp';
+import './App1.css';
+
+function App() {
+  return (
+    <>
+   <h1 id="hello">Hello</h1>
+   <h2>App me Add kiya</h2>
+   <Comp />
+   </>
+  );
+}
+
+export default App;
---
+++ src/comp.js
@@ -0,0 +1,11 @@
+function Comp(){
+    return(
+        <>
+        <h3>Heyy Guyss</h3>
+        <p>Welcome To React World</p>
+        </>
+        
+    )
+};
+
+export default Comp;
